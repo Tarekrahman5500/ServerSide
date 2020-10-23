3f290ad28de6b6a21106e63138bd6306713755b7
Tarek Rahman <tr617739@gmail.com>
2020-10-24 00:43:55 +0600

    Assignment 1

--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -17,12 +17,34 @@ dishRouter.route('/')
     .post( (req, res, next) => {
         res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description); // carry info
     })
-    .put( (req, res, next) => {
+    .put((req, res, next) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /dishes');
     })
-    .delete( (req, res, next) => {
+    .delete((req, res, next) => {
         res.end('Deleting all dishes');
+    });
+
+// the /: is start here
+dishRouter.route('/:dishId')
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        next();
+    })
+    .get((req, res, next) => {
+        res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+    })
+    .post((req, res, next) => {
+        res.end('POST operation not supported on /dishes' + req.params.dishId);
+    })
+    .put((req, res, next) => {
+        res.write('Updating the dish: ' + req.params.dishId + '\n');
+        res.end('Will update the dish: ' + req.body.name +
+            ' with details: ' + req.body.description);
+    })
+    .delete( (req, res, next) => {
+        res.end('Deleting dish: ' + req.params.dishId);
     });
 
 module.exports = dishRouter;
