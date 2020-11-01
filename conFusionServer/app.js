0b177dee29ecbdb661db33b57b4dcb5e7e0c9d27
Tarek Rahman <tr617739@gmail.com>
2020-11-02 00:33:44 +0600

    Express Session Part 2

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -1,86 +1,69 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const session =  require('express-session');
-const FileStore = require('session-file-store')(session);
+const express = require("express");
+const path = require("path");
+const favicon = require("static-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const app = express();
+const dishRouter = require("./routes/dishRouter");
+const promoRouter = require("./routes/promoRouter");
+const leaderRouter = require("./routes/leaderRouter");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const dishRouter = require('./routes/dishRouter');
-const leaderRouter = require('./routes/leaderRouter');
-const promoRouter = require('./routes/promoRouter');
-
-const mongoose = require('mongoose');
-
-const Dishes = require('./models/dishes');
-
-const url = 'mongodb://localhost:27017/conFusion';
+const mongoose = require("mongoose");
+const Dishes = require("./models/dishes");
+const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
 
-connect.then((db) => {
-    console.log("Connected correctly to server");
-}, (err) => {
-    console.log(err);
-});
-
-const app = express();
+connect.then(
+    db => {
+        console.log("Connected correctly to server");
+    },
+    err => {
+        console.log(err);
+    }
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-//app.use(cookieParser('12345-67890-09876-54321'));
+app.use(favicon());
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+// app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(session({
-    name: 'session-id',
-    secret: '12345-67890-09876-54321',
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore()
-}));
+app.use(
+    session({
+        name: "session-id",
+        secret: "12345-67890-09876-54321",
+        saveUninitialized: false,
+        resave: false,
+        store: new FileStore()
+    })
+);
 
-function auth(req, res, next) {
-    console.log(session);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-    if (!req.session.user) {
+function auth (req, res, next) {
+    let err;
+    console.log(req.session);
 
-        let authHeader = req.headers.authorization;
-
-        if (!authHeader) {
-            let err = new Error("You are not authenticated!");
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
-        // [0] contain base [1] contain array two split to separate ser and pass
-
-        let auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-        let username = auth[0];
-        let password = auth[1];
-
-        if (username === 'admin' && password === 'password') {
-             req.session.user = 'admin';
-            // next() call next middleware
-            next();
-        } else {
-            let err = new Error("You are not authenticated!");
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            return next(err);
-        }
-    } else {
-        if (req.session.user === 'admin') {
-            next();
-        } else {
-            let err = new Error("You are not authenticated!");
-           // res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
+    if(!req.session.user) {
+        err = new Error('You are not authenticated!');
+        err.status = 403;
+        return next(err);
+    }
+    else {
+        if (req.session.user === 'authenticated') next();
+        else {
+            err = new Error('You are not authenticated!');
+            err.status = 403;
             return next(err);
         }
     }
@@ -88,28 +71,48 @@ function auth(req, res, next) {
 
 app.use(auth);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/dishes', dishRouter);
-app.use('/leaders', leaderRouter);
-app.use('/promotions', promoRouter);
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
 });
 
-// error handler
-app.use(function (err, req, res, next) {
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get("env") === "development") {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render("error", {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    // next is the last argument in above function
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render("error", {
+        message: err.message,
+        error: {}
+    });
 });
+
 
 module.exports = app;
